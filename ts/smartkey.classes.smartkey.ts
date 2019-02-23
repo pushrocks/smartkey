b3d1dfc60764e593dacc8d1398648f2220d8868d
Phil Kunz <github@philkunz.com>
2019-02-23 16:09:57 +0100

    fix(core): update

--- ts/smartkey.classes.smartkey.ts
+++ ts/smartkey.classes.smartkey.ts
@@ -3,7 +3,7 @@ import { KeyPair } from './smartkey.classes.keypair';
 
 export class SmartKey {
   // instance
-  public async getKeypair(passohrase?: string): Promise<KeyPair> {
-    return KeyPair.createKeyPair();
+  public async getKeypair(passphraseArg?: string): Promise<KeyPair> {
+    return KeyPair.createKeyPair(passphraseArg);
   }
 }
